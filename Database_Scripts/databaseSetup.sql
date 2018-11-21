0745b68e848b469ce5f9e1ce4bc026c8935078a2
Ash Ezani <wanahmaw@oregonstate.edu>
2018-11-20 23:16:48 -0800

    Added table for user & games
    Table shows relationship between users in a game. In a game, can add user, remove user, & show all users participating

--- Database_Scripts/databaseSetup.sql
+++ Database_Scripts/databaseSetup.sql
@@ -44,3 +44,30 @@ INSERT INTO users (user_name, user_password) VALUES ('TestUser1', 'sports!');
 #Add a game
 
 INSERT INTO games (sport_type, start_date, start_time, host_user, max_players, current_players, location_name, location_address, location_city, location_state, location_zip, location_lat, location_long) VALUES ('Football', '2018-11-19', '15:30:00', 1, 30, 3, 'Football Field', '123 Some Street', 'Philadelphia', 'PA', '19019', 39.95233, -75.16379);
+
+#Create users in game table (N-M)
+
+CREATE TABLE gameUsers (
+  `gameUsers_id`SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `game_id` SMALLINT UNSIGNED NOT NULL,
+  `user_id` SMALLINT UNSIGNED,
+  PRIMARY KEY (gameUsers_id),
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (user_id) 
+  ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT fk_game_id FOREIGN KEY (game_id) REFERENCES games (game_id) 
+  ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# Add user into game
+INSERT INTO gameUsers (game_id, user_id) VALUES (1, 1);
+
+# Remove user from game
+DELETE FROM gameUsers
+WHERE user_id = '1'
+AND game_id = '1'
+
+# Show all users in game
+SELECT u.user_name AS Names
+FROM users u
+INNER JOIN gameUsers g ON g.user_id = u.user_id
+WHERE g.game_id = '1';
